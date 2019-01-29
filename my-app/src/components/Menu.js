c5e3a99472015e98e40814709b5d03b4446c1844
dima <dgerasimovic551@gmail.com>
2019-01-30 01:01:34 +0300

    change

--- my-app/src/components/Menu.js
+++ my-app/src/components/Menu.js
@@ -1,40 +1,59 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './Menu.css';
 
 
-class Menu extends Component {
-    render() {
-        return (
-                <div className="nav-menu">
-                    <div className='nav-item'>
-                        <div>
-                        <a href='#'>
-                        Clock
-                        </a>
-                        </div>
-                        <div>
-                            adsadadasadass
-                        </div>
-                    </div>
-                    <div className='nav-item'>
-                        <a href='#'>
-                        Form
-                        </a>
-                    </div>
-                    <div className='nav-item'>
-                        <a href='#'>
-                        Link
-                        </a>
-                    </div>
-                    <div className='nav-item'>
-                        <a href='#'>
-                        Drop down
-                        </a>
-                    </div>
+const Menu = () => {
 
+    toggleMenu() {
+        
+    };
+        return(
+        <div className="nav-menu">
+            <div className='nav-block'>
+                <div className='nav-item'>
+                    <a href='#'>
+                        Clock
+                    </a>
                 </div>
+            </div>
+            <div className='nav-block'>
+                <div className='nav-item'>
+                    <a href='#'>
+                        Form
+                    </a>
+                </div>
+            </div>
+            <div className='nav-block'>
+                <div className='nav-item'>
+                    <a href='#'>
+                        Link
+                    </a>
+                </div>
+            </div>
+            <div className='nav-block'>
+                <div className='nav-item'>
+                    <a href='#' onClick={this.toggleMenu.bind(this)}>
+                        Drop down
+                    </a>
+                </div>
+                <div className='drop-down'>
+                    <div>
+                        ReactJS
+                    </div>
+                    <div>
+                        Native JS
+                    </div>
+                    <div>
+                        CSS
+                    </div>
+                    <div>
+                        HTML
+                    </div>
+                </div>
+            </div>
+
+        </div>
         );
-    }
 }
 
 export default Menu;
